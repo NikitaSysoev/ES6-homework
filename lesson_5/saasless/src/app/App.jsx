92161c7bd591881986c48ac49c82f0c49eaccd0d
NikitaSysoev <na.sysoev@gmail.com>
2018-09-27 21:50:47 +0300

    Adding scss-forms

---
+++ lesson_5/saasless/src/app/App.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+import './App.scss';
+import './custom.scss';
+
+// import LoginForm from '../../../ssls-login-form/src/LoginForm/LoginForm';
+// import '../../../ssls-login-form/src/LoginForm/LoginForm.scss';
+
+
+import LoginForm from 'ssls-login-form';
+// import 'ssls-login-form/dist/css/bundle.css';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="ssls-app">
+                <header className="ssls-header"/>
+                <div className="ssls-content">
+                    <LoginForm/>
+                </div>
+                <footer className="ssls-footer"/>
+            </div>
+        )
+    }
+}
---
+++ lesson_5/ssls-login-form/rollup.config.js
@@ -0,0 +1,50 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import scss from 'rollup-plugin-scss';
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
+
+export default {
+    external: ['react'],
+    input: 'src/LoginForm/LoginForm.jsx',
+    output: {
+        file: 'dist/index.js',
+        format: 'es',
+        name: 'LoginForm',
+        sourceMap: 'inline',
+        globals: {
+            'react': 'React'
+        },
+    },
+    plugins: [
+        globals(),
+        builtins(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        scss({
+            output: 'dist/css/bundle.css',
+        }),
+        resolve({
+            jsnext: true,
+            main: true,
+            browser: true,
+            extensions: ['.jsx', '.js'],
+        }),
+        commonjs(
+            {
+                include: 'node_modules/**',
+                namedExports: {
+                    'node_modules/react/index.js': ['Component']
+                }
+            }
+        ),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        uglify(),
+    ],
+};
